7990d610e6f90f87f748fac89397a8aa6c4bd52b
Faguim02 <fagnermuniz12@gmail.com>
2026-01-05 15:03:27 -0300

    refactor: alterando o arquivo de configuração prisma

--- backend/src/config/router.js
+++ backend/src/config/router.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import { UsuarioController } from "../controller/UsuarioContorller.js";
 
 export const router = Router();
 
-router.get('/', (req, res) => {
+router.get('', (req, res) => {
   res.json({ message: 'Express server is running 🚀' })
 });
+
+const usuarioController = new UsuarioController();
+
+router.post('/api/usuarios', usuarioController.createUser.bind(usuarioController));
