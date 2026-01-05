49d3d3ff959e9ce7b712083819f78a58552ff544
Faguim02 <fagnermuniz12@gmail.com>
2026-01-05 15:06:41 -0300

    refactor: alterando service

--- backend/src/service/CronogramaService.js
+++ backend/src/service/CronogramaService.js
@@ -1,11 +1,4 @@
 export class CronogramaService {
-  constructor(prisma) {
-    this.prisma = prisma;
-  }
-
-  async getAllCronogramas() {
-    return this.prisma.cronograma.findMany();
-  }
-
+  
   
 }
--- backend/src/service/UsuarioService.js
+++ backend/src/service/UsuarioService.js
@@ -1,11 +1,22 @@
+import { hash } from 'bcrypt';
+import { prisma } from '../config/db.js';
+
 export class UsuarioService {
-  constructor(prisma) {
-    this.prisma = prisma;
-  }
 
-  async getAllUsers() {
-    return this.prisma.usuario.findMany();
-  }
+  async createUser(data) {
+    console.log('Service: ', data);
+    const userExists = await prisma.usuario.findUnique({ where: { email: data.email } });
+    console.log(userExists);
+    if (userExists != null) {
+      throw new Error('Usuário já cadastrado');
+    }
 
-  
+    console.log('Hashing password...');
+
+    const passwordHash = await hash(data.password, 10);
+    console.log('Password hashed.', passwordHash);
+    data.password = passwordHash;
+
+    return prisma.usuario.create({ data });
+  }
 }
