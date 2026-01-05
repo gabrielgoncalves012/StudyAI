b47fab6e6d4a4040cb52ba82c8993edde310ce44
Faguim02 <fagnermuniz12@gmail.com>
2026-01-05 15:04:15 -0300

    fix: corrigindo ordem dos comandos

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { router } from './src/config/router.js'
+import e from 'express'
 
 const app = express()
 
@@ -9,10 +10,10 @@ app.use(cors({
   origin: 'http://localhost:5173', // porta do Vite
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
-app.use(router);
-// Middlewares
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(router);
 
 
 const PORT = process.env.PORT || 3333
