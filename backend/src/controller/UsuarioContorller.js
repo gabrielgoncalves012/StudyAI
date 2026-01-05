fbc5136d55b51322df7b86b3ff1c72feb3f28c03
Faguim02 <fagnermuniz12@gmail.com>
2026-01-05 15:06:10 -0300

    refactor: alterando o controller

---
+++ backend/src/controller/UsuarioContorller.js
@@ -0,0 +1,18 @@
+import { UsuarioService } from '../service/UsuarioService.js';
+
+export class UsuarioController {
+  constructor(usuarioService) {
+    this.usuarioService = new UsuarioService();
+  }
+
+  async createUser(req, res) {
+    try {
+      console.log('Controller: ', req.body);
+      await this.usuarioService.createUser(req.body);
+      console.log('User created successfully.');
+      return res.status(201).json({ message: 'Usuário criado com sucesso' });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
+}
