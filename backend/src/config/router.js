fb3cf0ff201ab236d9124744317e76b90b9f5b67
Faguim02 <fagnermuniz12@gmail.com>
2026-01-06 15:38:44 -0300

    feat: adicionando rota de teste

--- backend/src/config/router.js
+++ backend/src/config/router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { UsuarioController } from "../controller/UsuarioContorller.js";
+import verify from "../middlewares/verifyJwt.js";
 
 export const router = Router();
 
@@ -7,3 +8,7 @@ const usuarioController = new UsuarioController();
 
 router.post('/api/usuarios', usuarioController.createUser.bind(usuarioController));
 router.post('/api/usuarios/signin', usuarioController.signIn.bind(usuarioController));
+
+router.get('/api/teste', verify, (req, res) => {
+    res.send('Rota protegida acessada com sucesso!');
+});
