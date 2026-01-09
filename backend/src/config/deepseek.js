8751a78db616389a7b7b859dcc1b148a92b32ece
Faguim02 <fagnermuniz12@gmail.com>
2026-01-09 12:21:23 -0300

    refactor: alterando tempo de execução

--- backend/src/config/deepseek.js
+++ backend/src/config/deepseek.js
@@ -1,18 +1,35 @@
 import OpenAI from "openai";
+import http from 'http'
 
 const openai = new OpenAI({
         baseURL: 'https://api.deepseek.com',
         apiKey: process.env.DEEPSEEK_API_KEY,
+        timeout: 300000,
         defaultHeaders: {
                 'Accept-Language': 'pt-BR',
+        },
+        httpAgent: new http.Agent({ 
+                keepAlive: true,
+                keepAliveMsecs: 60000,
+                timeout: 0 // Desativa timeout do socket
+        }),
+        
+        fetch: async (url, options) => {
+                const controller = new AbortController();
+                const timeoutId = setTimeout(() => controller.abort(), 280000); // 4:40min
+                
+                try {
+                        const response = await fetch(url, {
+                        ...options,
+                        signal: controller.signal
+                        });
+                        clearTimeout(timeoutId);
+                        return response;
+                } catch (error) {
+                        clearTimeout(timeoutId);
+                        throw error;
+                }
         }
 });
 
 export { openai };
-
-// const completion = await openai.chat.completions.create({
-//     messages: [{ role: "system", content: "You are a helpful assistant." }],
-//     model: "deepseek-chat",
-//   });
-
-//   console.log(completion.choices[0].message.content);
