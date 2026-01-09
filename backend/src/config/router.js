90d5eb09bbde5dc88bcab4c72a7190b2c61a20bd
Faguim02 <fagnermuniz12@gmail.com>
2026-01-09 12:22:53 -0300

    feat: adicionando rota para gerar questões

--- backend/src/config/router.js
+++ backend/src/config/router.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { UsuarioController } from "../controller/UsuarioContorller.js";
 import verify from "../middlewares/verifyJwt.js";
+import { QuestoesController } from "../controller/QuestoesController.js";
 
 export const router = Router();
 
 const usuarioController = new UsuarioController();
+const questoesController = new QuestoesController();
 
 router.post('/api/usuarios', usuarioController.createUser.bind(usuarioController));
 router.post('/api/usuarios/signin', usuarioController.signIn.bind(usuarioController));
 
-router.get('/api/teste', verify, (req, res) => {
-    res.send('Rota protegida acessada com sucesso!');
-});
+router.post('/api/generate-question', verify, questoesController.generateQuestion.bind(questoesController));
