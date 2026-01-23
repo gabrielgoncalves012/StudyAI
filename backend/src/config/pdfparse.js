415d87b9850deefce782ef57fe87399306142082
Faguim02 <fagnermuniz12@gmail.com>
2026-01-23 11:43:22 -0300

    feat: configurando o pdfParse

---
+++ backend/src/config/pdfparse.js
@@ -0,0 +1,25 @@
+import { PDFParse } from 'pdf-parse';
+
+
+async function extractTextFromPDF(filePath) {
+
+    if (!filePath) {
+        throw new Error('File path is required');
+    }
+
+    if (typeof filePath !== 'string') {
+        
+        if (!filePath.startsWith('https://')) {
+            
+            const pdfParse = new PDFParse({url: filePath, verbosity: 0});
+            return (await pdfParse.getText()).text;
+
+        }
+
+    }
+    
+    const pdfParse = new PDFParse({data: filePath, verbosity: 0});
+    return (await pdfParse.getText()).text;
+}
+
+export { extractTextFromPDF };
