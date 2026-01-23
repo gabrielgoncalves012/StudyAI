e37b6a64503b0e2d25a8496aaeb235467e82112e
Faguim02 <fagnermuniz12@gmail.com>
2026-01-23 11:44:02 -0300

    feat: adiconando a rota que cria o cronograma

--- backend/src/config/router.js
+++ backend/src/config/router.js
@@ -2,13 +2,18 @@ import { Router } from "express";
 import { UsuarioController } from "../controller/UsuarioContorller.js";
 import verify from "../middlewares/verifyJwt.js";
 import { QuestoesController } from "../controller/QuestoesController.js";
+import upload from "./multer.js";
+import { CronogramaController } from "../controller/CronogramaController.js";
 
 export const router = Router();
 
 const usuarioController = new UsuarioController();
 const questoesController = new QuestoesController();
+const cronogramaController = new CronogramaController();
 
 router.post('/api/usuarios', usuarioController.createUser.bind(usuarioController));
 router.post('/api/usuarios/signin', usuarioController.signIn.bind(usuarioController));
 
 router.post('/api/generate-question', verify, questoesController.generateQuestion.bind(questoesController));
+
+router.post('/api/cronograma', upload.single('file'), cronogramaController.generateCronograma.bind(cronogramaController));
