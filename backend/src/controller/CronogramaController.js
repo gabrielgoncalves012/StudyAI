7b7669dd0e0df754b33f7df939e19d1c85527f70
Faguim02 <fagnermuniz12@gmail.com>
2026-01-23 11:46:56 -0300

    em construção

--- backend/src/controller/CronogramaController.js
+++ backend/src/controller/CronogramaController.js
@@ -2,7 +2,21 @@ import { CronogramaService } from "../service/CronogramaService.js";
 
 export class CronogramaController {
   // Cronograma controller methods would go here
-  constructor(cronogramaService) {
-    this.cronogramaService = new CronogramaService();
+  async generateCronograma(req, res) {
+
+
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: 'Nenhum arquivo PDF foi enviado'
+      });
+    }
+
+    console.log(req.file.originalname)
+    console.log(req.body);
+
+    const cronogramaService = new CronogramaService();
+    const response = await cronogramaService.createCronograma(req.body, req.file);
+    res.status(200).json("Cronograma criado com sucesso");//.json(response);
   }
 }
