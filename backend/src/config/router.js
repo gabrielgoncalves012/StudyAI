01a998f60960ee7d7ce724c07a2c533a3d5c0337
Faguim02 <fagnermuniz12@gmail.com>
2026-01-25 20:41:26 -0300

    feat: adiconando rotas: 'findAllCronogrmas', 'findCronogramaById', 'check', 'edit', 'delete'

--- backend/src/config/router.js
+++ backend/src/config/router.js
@@ -17,3 +17,8 @@ router.post('/api/usuarios/signin', usuarioController.signIn.bind(usuarioControl
 router.post('/api/generate-question', verify, questoesController.generateQuestion.bind(questoesController));
 
 router.post('/api/cronograma', upload.single('file'), cronogramaController.generateCronograma.bind(cronogramaController));
+router.get('/api/cronogramas', cronogramaController.findAllCronogramas.bind(cronogramaController));
+router.get('/api/cronograma/:id', cronogramaController.findCronogramaById.bind(cronogramaController));
+router.get('/api/cronograma/topico/:topicoId', cronogramaController.checkTopicCompletion.bind(cronogramaController));
+router.get('/api/cronograma/:id', cronogramaController.editCronograma.bind(cronogramaController));
+router.delete('/api/cronograma/:id', cronogramaController.deleteCronograma.bind(cronogramaController));
