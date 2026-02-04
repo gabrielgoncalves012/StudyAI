2dcbf611020a2a33359f3ee3caca9601444e2708
Faguim02 <fagnermuniz12@gmail.com>
2026-02-04 13:43:28 -0300

    feat: adicionando novo /

--- backend/src/config/router.js
+++ backend/src/config/router.js
@@ -16,6 +16,8 @@ router.post('/api/usuarios/signin', usuarioController.signIn.bind(usuarioControl
 router.post('/api/usuarios/verify', usuarioController.verifyEmail.bind(usuarioController));
 
 router.post('/api/generate-question', verify, questoesController.generateQuestion.bind(questoesController));
+router.get('/api/disciplines', questoesController.findAllDiscpipline.bind(questoesController));
+router.post('/api/questions', questoesController.findAllTopicsByDiscipline.bind(questoesController));
 
 router.post('/api/cronograma', verify, upload.single('file'), cronogramaController.generateCronograma.bind(cronogramaController));
 router.get('/api/cronogramas', verify, cronogramaController.findAllCronogramas.bind(cronogramaController));
