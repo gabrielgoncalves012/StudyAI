c74fc1eb4cc89ded682233267c25a8a44e89bfaf
Faguim02 <fagnermuniz12@gmail.com>
2026-02-11 10:38:16 -0300

    feat: adiconando serviço de api

---
+++ frontend/src/services/CronogramaService/CronogramService.js
@@ -0,0 +1,28 @@
+import { api, useJwtToken } from '../Api.js';
+
+export class CronogramaService {
+    async findAllCronogramas() {
+        try {
+            useJwtToken(); // Ensure the JWT token is set in the headers
+            const response = await api.get('/cronogramas/');
+            console.log('Response data:', response.data);
+            return response.data;
+
+        } catch (error) {
+            
+        }
+    }
+    async createCronograma(cronograma) {
+        try {
+            const response = await api.post('/cronograma/', cronograma, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error creating cronograma:', error);
+            throw error;
+        }
+    }
+}
