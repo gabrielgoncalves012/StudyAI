78f79adc60f847dced6414cebc8120885e3a63ee
Faguim02 <fagnermuniz12@gmail.com>
2026-02-11 12:49:06 -0300

    adiconando reactquery

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
+import { QueryApp } from './context/QueryClient'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryApp>
       <App />
+    </QueryApp>
   </React.StrictMode>
 )
