10184a54c9366bd8195d6de6c1a496d31db5d51d
Faguim02 <fagnermuniz12@gmail.com>
2026-02-13 11:33:25 -0300

    feat: adiconando cronograma semanal

--- frontend/src/pages/Cronograma.jsx
+++ frontend/src/pages/Cronograma.jsx
@@ -1,14 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { CronogramaService } from "../services/CronogramaService/CronogramService";
-import { IoBookOutline, IoTimeOutline } from "react-icons/io5";
+import { IoBookOutline, IoTimeOutline, IoCalendarClearOutline } from "react-icons/io5";
 import { FiTarget } from "react-icons/fi";
 import { GrBook } from "react-icons/gr";
-import { GoTrophy } from "react-icons/go";
+import { GoTrophy, GoChevronLeft, GoChevronRight } from "react-icons/go";
 import { PiChartLineUp } from "react-icons/pi";
 import '../styles/cronograma.css'
 import ProgressBar from "../components/ProgressBar";
 import { useEffect, useState } from "react";
+import WeeklyPlanner from "../components/WeeklyPlanner";
 
 export default function Cronograma() {
     const {id} = useParams()
@@ -101,6 +102,32 @@ export default function Cronograma() {
                                 <span>Progresso</span>
                             </div>
                         </section>
+                </article>
+
+                <article className="cronograma-container">
+                    <div className="header-cronograma-container">
+                        <div className="start-container">
+                            <div className="icon">
+                                <IoCalendarClearOutline/>
+                            </div>
+                            <div>
+                                <h2 style={{'margin-bottom': '5px'}}>Cronograma semanal</h2>
+                                <span>Planejamento de x semanas</span>
+                            </div>
+                        </div>
+                        <div className="end-container">
+                            <button>
+                                <GoChevronLeft/>
+                            </button>
+                            <span>Samana x</span>
+                            <button>
+                                <GoChevronRight/>
+                            </button>
+                        </div>
+                    </div>
+
+                     <WeeklyPlanner planejamentos={data.planejamentos}/>
+
                 </article>
 
             </main>
