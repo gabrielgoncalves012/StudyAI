97fa07e630a6a2d3ec0ef1aa9c237e7b3e6e87c0
Faguim02 <fagnermuniz12@gmail.com>
2026-02-13 10:19:29 -0300

    feat: adiconando nova rota para o cronograma

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,12 @@ import Dashboard from './pages/Dashboard'
 import PrivateRoute from './components/PrivateRoute'
 import Register from './pages/Register'
 import SelectConcurso from './pages/SelectConcurso'
+import Cronograma from './pages/Cronograma'
 
 function App() {
+
+  
+
   return (
     <BrowserRouter>
       <Routes>
@@ -14,6 +18,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/selecionar-concurso" element={<SelectConcurso />} />
+        <Route  path='/cronograma/:id' element={<Cronograma/>}/>
         <Route
           path="/dashboard"
           element={
