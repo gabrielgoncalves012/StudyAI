6b18b236a47efe661b7fcb39c79d3aab179e1645
Faguim02 <fagnermuniz12@gmail.com>
2026-02-19 15:29:04 -0300

    feat: função de desativar cliente

--- backend/src/module/pagamento/service/ClientStripeService.js
+++ backend/src/module/pagamento/service/ClientStripeService.js
@@ -5,6 +5,29 @@ export class ClientStripeService {
 
     async createClientStripe(customer) {
         try {
+
+            const existingClient = await prisma.clientStripe.findFirst({
+                where: {
+                    email: customer.email,
+                }
+            })
+
+            if (existingClient) {
+                
+                await prisma.clientStripe.update({
+                    where: {
+                        id: existingClient.id,
+                    },
+                    data: {
+                        actived: true,
+                    }
+                })
+
+                return {
+                    message: 'Cliente já existe, mas foi reativado',
+                }
+
+            }
             
             const stripeCustomer = await stripeClient.customers.create({
                 name: customer.name,
@@ -27,6 +50,38 @@ export class ClientStripeService {
                 error: error.message,
             }
         }
+    }
+
+    async desativeClientStripe(userId) {
+        try {
+            const clientStripe = await prisma.clientStripe.findFirst({
+                where: {
+                    userId: userId,
+                    actived: true,
+                }
+            })
+
+            if (!clientStripe) {
+                return {
+                    message: 'Cliente não encontrado',
+                }
+            }
+
+            await prisma.clientStripe.update({
+                where: {
+                    id: clientStripe.id,
+                },
+                data: {
+                    actived: false,
+                }
+            })
+        } catch (error) {
+            return {
+                message: 'Erro ao deletar cliente no Stripe',
+                error: error.message,
+            }
+        }
+
     }
 
 }
