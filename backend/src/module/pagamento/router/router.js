4e4cec97e1ac96a24f88a87f133f790e68e4bd76
Faguim02 <fagnermuniz12@gmail.com>
2026-02-22 23:03:59 -0300

    feat: adicionando rotas

--- backend/src/module/pagamento/router/router.js
+++ backend/src/module/pagamento/router/router.js
@@ -1,10 +1,17 @@
-import { Router } from 'express'
+import e, { Router } from 'express'
+import express from 'express'
 import verifyAdmin from '../../../shared/middlewares/verifyAdmin.js'
+import eventEmitter from '../../../shared/EventEmiter.js'
 //import verifyJwt from '../../../shared/middlewares/verifyJwt.js'
 
 import PlanController from '../controller/PlanController.js'
+import CheckoutController from '../controller/CheckoutController.js'
+import ClientStripeService from '../service/ClientStripeService.js'
+import { handleWebhook } from '../webhooks/StripeWebHook.js'
 
 const planController = new PlanController()
+const checkout = new CheckoutController()
+const clienteService = new ClientStripeService()
 
 const router = Router()
 
@@ -14,6 +21,14 @@ router.get('/plan', (req, res) => {
 
 router.post('/plan', verifyAdmin, planController.createPlan.bind(planController))
 router.get('/plans', planController.findAllPlans.bind(planController))
+
+router.post('/checkout', checkout.generateLink.bind(checkout))
+
+router.post('/webhook/stripe', handleWebhook)
+
+eventEmitter.on('user.created', async (user) => {
+    await clienteService.createClientStripe(user.userId)
+})
 
 export default {
     router
